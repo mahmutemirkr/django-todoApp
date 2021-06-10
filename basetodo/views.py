f39b5a0044ac45105f3520c3a0d7848d928d1fca
emir <mahmutemirkr@gmail.com>
2021-06-10 12:16:32 +0300

    views update

--- basetodo/views.py
+++ basetodo/views.py
@@ -2,7 +2,7 @@ from basetodo.models import Task
 from django.db import models
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic.list import ListView, T
+from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView,UpdateView,DeleteView
 from django.urls import reverse_lazy
@@ -37,3 +37,4 @@ class TaskDelete(DeleteView):
     model = Task
     context_object_name ="task"
     success_url = reverse_lazy('tasks')
+
