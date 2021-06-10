e2ec83efb52b87b5b9ed2a18fd2868d10f50c474
emir <mahmutemirkr@gmail.com>
2021-06-10 14:52:43 +0300

    v3

--- basetodo/views.py
+++ basetodo/views.py
@@ -6,7 +6,18 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView,UpdateView,DeleteView
 from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView
 from .models import Task
+
+
+class CustomLoginView(LoginView):
+    template_name = 'basetodo/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('tasks')
+
 
 
 class TaskList(ListView):
