b15e7bd590faec15f210cc289b29ea40eb2e9dc7
emir <mahmutemirkr@gmail.com>
2021-06-10 16:01:43 +0300

    v4

--- basetodo/views.py
+++ basetodo/views.py
@@ -6,7 +6,9 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView,UpdateView,DeleteView
 from django.urls import reverse_lazy
+
 from django.contrib.auth.views import LoginView
+
 from .models import Task
 
 
